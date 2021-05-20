0e007d620e079568b1eaf436e4c4cd1d16707087
DanielCloud9 <=>
2021-05-20 13:17:17 +0000

    JSON.parse on nil value causes issues in production

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,12 @@ class HomeController < ApplicationController
     @random_header = Item.offset(offset).first
     @popular_items = Item.order(:popularity)
     @saved_list_cookies = cookies[:saved_list]
-    @saved_list_cookies_array = JSON.parse(@saved_list_cookies)
+
+    if (@saved_list_cookies == nil)
+      saved_list_cookies_array = []
+    else
+      @saved_list_cookies_array = JSON.parse(@saved_list_cookies)
+    end
 
   end
 
